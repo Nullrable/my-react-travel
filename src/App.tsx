f0c5ecfdd81032eb2cdadaf5bba1a45edce2a6c4
lusudong <nhsoft.lsd@hotmail.com>
2022-02-10 16:41:57 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import 'antd/dist/antd.css';
+import {Layout, Typography, Dropdown, Menu, Button, Input} from 'antd'
+import {GlobalOutlined} from '@ant-design/icons'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <div>
+                {/* top-header */}
+                <div>
+                    <div className={styles["app-header"]}>
+
+                        <div className={styles["top-header"]}>
+                            <div className={styles.inner}>
+                                <Typography.Text style={{float: "left"}}>让旅游更幸福</Typography.Text>
+                                <Dropdown.Button
+                                    style={{marginLeft: 15, float: "left", marginTop: "5px"}}
+                                    overlay={
+                                        <Menu>
+                                            <Menu.Item>中文</Menu.Item>
+                                            <Menu.Item>English</Menu.Item>
+                                        </Menu>
+                                    }
+                                    icon={<GlobalOutlined/>}
+                                >
+                                    语言
+                                </Dropdown.Button>
+
+                                <Button.Group style={{float: "right", marginTop: "5px"}}>
+                                    <Button>注册</Button>
+                                    <Button>登录</Button>
+                                </Button.Group>
+                            </div>
+                        </div>
+                        <Layout.Header className={styles["main-header"]}>
+                            <img src={logo} alt="logo" className={styles["App-logo"]}/>
+                            <Typography.Title level={3} className={styles.title}>
+                                React旅游网
+                            </Typography.Title>
+                            <Input.Search
+                                placeholder={"请输入旅游目的地、主题、或关键字"}
+                                className={styles["search-input"]}
+                            />
+                        </Layout.Header>
+
+                    </div>
+
+                    <Menu mode={"horizontal"} className={styles["main-menu"]}>
+                        <Menu.Item key={1}>旅游首页</Menu.Item>
+                        <Menu.Item key={2}>周末游</Menu.Item>
+                        <Menu.Item key={3}>跟团游</Menu.Item>
+                        <Menu.Item key="4"> 自由行 </Menu.Item>
+                        <Menu.Item key="5"> 私家团 </Menu.Item>
+                        <Menu.Item key="6"> 邮轮 </Menu.Item>
+                        <Menu.Item key="7"> 酒店+景点 </Menu.Item>
+                        <Menu.Item key="8"> 当地玩乐 </Menu.Item>
+                        <Menu.Item key="9"> 主题游 </Menu.Item>
+                        <Menu.Item key="10"> 定制游 </Menu.Item>
+                        <Menu.Item key="11"> 游学 </Menu.Item>
+                        <Menu.Item key="12"> 签证 </Menu.Item>
+                        <Menu.Item key="13"> 企业游 </Menu.Item>
+                        <Menu.Item key="14"> 高端游 </Menu.Item>
+                        <Menu.Item key="15"> 爱玩户外 </Menu.Item>
+                        <Menu.Item key="16"> 保险 </Menu.Item>
+                    </Menu>
+                </div>
+                <Layout.Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
