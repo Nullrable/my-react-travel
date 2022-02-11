f855f7eeec52e915d3f7295f17f9553b386443c3
lusudong <nhsoft.lsd@hotmail.com>
2022-02-11 15:35:31 +0800

    component module

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
 import 'antd/dist/antd.css';
-import {Layout, Typography, Dropdown, Menu, Button, Input} from 'antd'
-import {GlobalOutlined} from '@ant-design/icons'
+import {Footer, Header, SideMenu, Carousel, ProductCollection} from "./components";
+import {Row, Col, Typography} from "antd";
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
+import { productList1, productList2, productList3 } from "./mockups";
 
 function App() {
     return (
         <div className={styles.App}>
             <div>
-                {/* top-header */}
-                <div>
-                    <div className={styles["app-header"]}>
-
-                        <div className={styles["top-header"]}>
-                            <div className={styles.inner}>
-                                <Typography.Text>让旅游更幸福</Typography.Text>
-                                <Dropdown.Button
-                                    style={{marginLeft: 15}}
-                                    overlay={
-                                        <Menu>
-                                            <Menu.Item>中文</Menu.Item>
-                                            <Menu.Item>English</Menu.Item>
-                                        </Menu>
-                                    }
-                                    icon={<GlobalOutlined/>}
-                                >
-                                    语言
-                                </Dropdown.Button>
-
-                                <Button.Group style={{float: "right", marginTop: "5px"}}>
-                                    <Button>注册</Button>
-                                    <Button>登录</Button>
-                                </Button.Group>
-                            </div>
-                        </div>
-                        <Layout.Header className={styles["main-header"]}>
-                            <img src={logo} alt="logo" className={styles["App-logo"]}/>
-                            <Typography.Title level={3} className={styles.title}>
-                                React旅游网
+                <Header/>
+                <div className={styles['page-content']}>
+                    <Row>
+                        <Col span={6}>
+                            <SideMenu/>
+                        </Col>
+                        <Col span={18}>
+                            <Carousel />
+                        </Col>
+                    </Row>
+                    <ProductCollection
+                        title={
+                            <Typography.Title level={3} type="warning">
+                                爆款推荐
                             </Typography.Title>
-                            <Input.Search
-                                placeholder={"请输入旅游目的地、主题、或关键字"}
-                                className={styles["search-input"]}
-                            />
-                        </Layout.Header>
-                    </div>
-                    <Menu mode={"horizontal"} className={styles["main-menu"]}>
-                        <Menu.Item key={1}>旅游首页</Menu.Item>
-                        <Menu.Item key={2}>周末游</Menu.Item>
-                        <Menu.Item key={3}>跟团游</Menu.Item>
-                        <Menu.Item key="4"> 自由行 </Menu.Item>
-                        <Menu.Item key="5"> 私家团 </Menu.Item>
-                        <Menu.Item key="6"> 邮轮 </Menu.Item>
-                        <Menu.Item key="7"> 酒店+景点 </Menu.Item>
-                        <Menu.Item key="8"> 当地玩乐 </Menu.Item>
-                        <Menu.Item key="9"> 主题游 </Menu.Item>
-                        <Menu.Item key="10"> 定制游 </Menu.Item>
-                        <Menu.Item key="11"> 游学 </Menu.Item>
-                        <Menu.Item key="12"> 签证 </Menu.Item>
-                        <Menu.Item key="13"> 企业游 </Menu.Item>
-                        <Menu.Item key="14"> 高端游 </Menu.Item>
-                        <Menu.Item key="15"> 爱玩户外 </Menu.Item>
-                        <Menu.Item key="16"> 保险 </Menu.Item>
-                    </Menu>
+                        }
+                        sideImage={sideImage}
+                        products={productList1}
+                    />
+                    <ProductCollection
+                        title={
+                            <Typography.Title level={3} type="danger">
+                                新品上市
+                            </Typography.Title>
+                        }
+                        sideImage={sideImage2}
+                        products={productList2}
+                    />
+                    <ProductCollection
+                        title={
+                            <Typography.Title level={3} type="success">
+                                国内游推荐
+                            </Typography.Title>
+                        }
+                        sideImage={sideImage3}
+                        products={productList3}
+                    />
                 </div>
-                <Layout.Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
+                <Footer/>
             </div>
         </div>
     );
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Layout} from "antd";
+
+export function Footer() {
+
+    return (
+        <Layout.Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
+    );
+}
