93d8f1316768149afdf73a8056aa276f500e39df
lusudong <nhsoft.lsd@hotmail.com>
2022-10-19 10:18:24 +0800

    add redux-persistent

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
 import {Provider} from "react-redux";
-import store from "./redux/store";
 import "./i18n/configs"
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistor, store} from "./redux/store"
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
             <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
